7f3ca2ec911eb8e01288fccba935104fe64911c6
JTantra <johandy_tantra@hotmail.com>
2025-05-06 09:54:03 +0800

    fix: configure middleware to exclude .swa/* routes

--- src/app/.swa/health/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export default function HealthPage() {
-  return (
-    <div>Hello</div>
-  )
-}
---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+console.log('Middleware loaded');
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - .swa (Azure Static Web Apps)
+     */
+    '/((?!.swa).*)',
+  ],
+}
