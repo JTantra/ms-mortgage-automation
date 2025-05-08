70920d7f7f41fd42fb22fe68adb71190bc7a3700
JTantra <johandy_tantra@hotmail.com>
2025-05-08 10:47:04 +0800

    build with API

---
+++ src/app/api/health/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+export const GET = async (req: NextRequest) => {
+  return NextResponse.json({
+    success: true,
+    message: "API Server running",
+  });
+};
